e7682b40de089c504f33a9c4205bd2e8d0b3d088
Elijus Juozas Ulickas <today9432@gmail.com>
2024-01-18 18:34:08 +0200

    Update package.json and functions.php

---
+++ wp-content/themes/influenceup/inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * InfluenceUp Theme Customizer
+ *
+ * @package InfluenceUp
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function influenceup_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'influenceup_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'influenceup_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'influenceup_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function influenceup_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function influenceup_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function influenceup_customize_preview_js() {
+	wp_enqueue_script( 'influenceup-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'influenceup_customize_preview_js' );
