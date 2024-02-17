a8bd6d2a8bdd31dce51d02754fb817e1a3d8f3dc
kestas97 <kestutissukys97@gmail.com>
2024-02-17 19:07:18 +0200

    updates

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define( 'WP_CACHE', true );
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'influeneceup' );
+define( 'DB_NAME', 'influenece' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
