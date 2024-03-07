1cf7492bf9f2288b5989c0963bf939845eb2060c
kestas97 <kestutissukys97@gmail.com>
2024-03-07 22:48:25 +0200

    return back wp-config

--- wp-config.php
+++ wp-config.php
@@ -22,16 +22,16 @@ define( 'WP_CACHE', true );
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'u981414503_karqY' );
+define( 'DB_NAME', 'influenece' );
 
 /** Database username */
-define( 'DB_USER', 'u981414503_Vg2vi' );
+define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'zhgdAFImcg' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', '127.0.0.1' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
