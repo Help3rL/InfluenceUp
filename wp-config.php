daec24f1970146c4a18e2735218b279729c49fa8
kestas97 <kestutissukys97@gmail.com>
2024-06-09 23:12:49 +0300

    changed DB login

--- wp-config.php
+++ wp-config.php
@@ -22,16 +22,16 @@ define( 'WP_CACHE', true );
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'influenece' );
+define( 'DB_NAME', 'u981414503_karqY' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'u981414503_Vg2vi' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'zhgdAFImcg' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '127.0.0.1' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
