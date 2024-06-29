8990923029a219ab7b029802fc7004c900ae7dc2
kestas97 <kestutissukys97@gmail.com>
2024-06-29 13:36:28 +0300

    listings page updates

---
+++ wp-content/themes/influenceup/js/rtcl-categories-carousel-listings.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function ($) {
+  $(".carousel-cards-listings").slick({
+    autoplay: false,
+    dots: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    prevArrow: $(".carousel-prev"),
+    nextArrow: $(".carousel-next"),
+    centerMode: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          centerMode: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          centerMode: false,
+        },
+      },
+    ],
+  });
+});
